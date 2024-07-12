edd3bef5a953eea7cf43e5c94ab42199c4271256
Sefry Syahrudin <sefry2693@gmail.com>
2024-07-12 17:48:09 +0700

    delete: searchbar in chat page

--- lib/features/chat/screen/chat_page.dart
+++ lib/features/chat/screen/chat_page.dart
@@ -16,158 +16,93 @@ class ChatPage extends StatefulWidget {
 
 class _ChatPageState extends State<ChatPage> {
   List<UserModel> _list = [];
-  List<UserModel> _searchList = [];
-  bool _isSearching = false;
-  
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      //for hiding keyboard when a tap is detected on screen
-      onTap: FocusScope.of(context).unfocus,
-      child: PopScope(
-        canPop: !_isSearching,
-        onPopInvoked: (_) async {
-          if (_isSearching) {
-            setState(() => _isSearching = !_isSearching);
-          } else {
-            Navigator.of(context).pop();
-          }
-        },
+    return Scaffold(
+      //app bar
+      appBar: AppBar(
+        backgroundColor: AppColors.primary,
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        leading: SMBackButton(
+          buttonColor: Colors.white,
+        ),
+        title: const Text(
+          'Pesan',
+          style: TextStyle(color: AppColors.white),
+        ),
+      ),
 
-        //
-        child: Scaffold(
-          //app bar
-          appBar: AppBar(
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: FloatingActionButton(
             backgroundColor: AppColors.primary,
-            automaticallyImplyLeading: false,
-            centerTitle: true,
-            leading: SMBackButton(
-              buttonColor: Colors.white,
-            ),
-            title: _isSearching
-                ? TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: 'Cari pengguna...',
-                        hintStyle: AppTextStyle.body3Regular
-                            .copyWith(color: AppColors.white)),
-                    autofocus: true,
-                    style: const TextStyle(fontSize: 17, letterSpacing: 0.5),
-                    //when search text changes then updated search list
-                    onChanged: (val) {
-                      //search logic
-                      _searchList.clear();
+            onPressed: () {
+              _addChatUserDialog(context);
+            },
+            child: const Icon(
+              Icons.person_add_outlined,
+              color: AppColors.neutral02,
+            )),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: StreamBuilder(
+          stream: ChatController.getMyUsersId(),
 
-                      for (var i in _list) {
-                        if (i.fullName
-                            .toLowerCase()
-                            .contains(val.toLowerCase())) {
-                          _searchList.add(i);
-                          setState(() {
-                            _searchList;
-                          });
+          //get id of only known users
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              //if data is loading
+              case ConnectionState.waiting:
+              case ConnectionState.none:
+                return const Center(child: CircularProgressIndicator());
+
+              //if some or all data is loaded then show it
+              case ConnectionState.active:
+              case ConnectionState.done:
+                return StreamBuilder(
+                  stream: ChatController.getAllUsers(
+                      snapshot.data?.docs.map((e) => e.id).toList() ?? []),
+
+                  //get only those user, who's ids are provided
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      //if data is loading
+                      case ConnectionState.waiting:
+                      case ConnectionState.none:
+                      // return const Center(
+                      //     child: CircularProgressIndicator());
+
+                      //if some or all data is loaded then show it
+                      case ConnectionState.active:
+                      case ConnectionState.done:
+                        final data = snapshot.data?.docs;
+                        _list = data
+                                ?.map((e) => UserModel.fromJson(e.data()))
+                                .toList() ??
+                            [];
+
+                        if (_list.isNotEmpty) {
+                          return ListView.builder(
+                              padding: EdgeInsets.symmetric(horizontal: 8),
+                              itemCount: _list.length,
+                              physics: const BouncingScrollPhysics(),
+                              itemBuilder: (context, index) {
+                                return ChatCard(user: _list[index]);
+                              });
+                        } else {
+                          return const Center(
+                            child: Text('Belum ada pesan!',
+                                style: AppTextStyle.body2Regular),
+                          );
                         }
-                      }
-                    },
-                  )
-                : const Text(
-                    'Pesan',
-                    style: TextStyle(color: AppColors.white),
-                  ),
-            actions: [
-              //search user button
-              IconButton(
-                  onPressed: () {
-                    setState(() {
-                      _isSearching = !_isSearching;
-                    });
+                    }
                   },
-                  icon: Icon(
-                    _isSearching
-                        ? CupertinoIcons.clear_circled_solid
-                        : Icons.search,
-                    color: AppColors.white,
-                  )),
-            ],
-          ),
-
-          floatingActionButton: Padding(
-            padding: const EdgeInsets.only(bottom: 10),
-            child: FloatingActionButton(
-                backgroundColor: AppColors.primary,
-                onPressed: () {
-                  _addChatUserDialog(context);
-                },
-                child: const Icon(
-                  Icons.person_add_outlined,
-                  color: AppColors.neutral02,
-                )),
-          ),
-          body: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: StreamBuilder(
-              stream: ChatController.getMyUsersId(),
-
-              //get id of only known users
-              builder: (context, snapshot) {
-                switch (snapshot.connectionState) {
-                  //if data is loading
-                  case ConnectionState.waiting:
-                  case ConnectionState.none:
-                    return const Center(child: CircularProgressIndicator());
-
-                  //if some or all data is loaded then show it
-                  case ConnectionState.active:
-                  case ConnectionState.done:
-                    return StreamBuilder(
-                      stream: ChatController.getAllUsers(
-                          snapshot.data?.docs.map((e) => e.id).toList() ?? []),
-
-                      //get only those user, who's ids are provided
-                      builder: (context, snapshot) {
-                        switch (snapshot.connectionState) {
-                          //if data is loading
-                          case ConnectionState.waiting:
-                          case ConnectionState.none:
-                          // return const Center(
-                          //     child: CircularProgressIndicator());
-
-                          //if some or all data is loaded then show it
-                          case ConnectionState.active:
-                          case ConnectionState.done:
-                            final data = snapshot.data?.docs;
-                            _list = data
-                                    ?.map((e) => UserModel.fromJson(e.data()))
-                                    .toList() ??
-                                [];
-
-                            if (_list.isNotEmpty) {
-                              return ListView.builder(
-                                  padding: EdgeInsets.symmetric(horizontal: 8),
-                                  itemCount: _isSearching
-                                      ? _searchList.length
-                                      : _list.length,
-                                  physics: const BouncingScrollPhysics(),
-                                  itemBuilder: (context, index) {
-                                    return ChatCard(
-                                        user: _isSearching
-                                            ? _searchList[index]
-                                            : _list[index]);
-                                  });
-                            } else {
-                              return const Center(
-                                child: Text('Belum ada pesan!',
-                                    style: AppTextStyle.body2Regular),
-                              );
-                            }
-                        }
-                      },
-                    );
-                }
-              },
-            ),
-          ),
+                );
+            }
+          },
         ),
       ),
     );
